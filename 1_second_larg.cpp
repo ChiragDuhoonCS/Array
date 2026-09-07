07284171fdb09553092bf5af5f282ec18ebb6ead
Chirag Duhoon <chiragduhoon.cs@gmail.com>
2026-09-07 19:10:29 +0530

    [fixed] optimize sol of sec largest

--- 1_second_larg.cpp
+++ 1_second_larg.cpp
@@ -1,8 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//> FINDING SECOND LARGEST
+
 void function1() {
-    int sl[] = {1,2,4,7,8,5};
+    int sl[] = {1,2,4,7,7,5};
 
     int largest, second_largest;
     largest = sl[0];
@@ -12,8 +14,13 @@ void function1() {
 
     for (int i = 0; i < n; i++)
     {
-        if (sl[i] > second_largest && sl[i] != largest )
+       if(sl[i] > largest) {
+        second_largest = largest; //! previous digit of largest goes into largeat when we found more largest
+        largest = sl[i];
+       }
+       else if(sl[i] > second_largest && sl[i] != largest) {
         second_largest = sl[i];
+       }
     }
     printf("%d", second_largest);
     
