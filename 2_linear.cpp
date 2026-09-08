070da3444b14a85bce561496295ff1a2f96da649
Chirag Duhoon <chiragduhoon.cs@gmail.com>
2026-09-08 08:25:43 +0530

    zero to end

--- 2_linear.cpp
+++ 2_linear.cpp
@@ -3,5 +3,14 @@ using namespace std;
 
 void program1() {
     int arr[] = {6,7,8,4,1};
-    int 
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int num;
+    
+
+    for (int i = 0; i < n; i++)
+    num = arr[i];
+    {
+        if (arr[i] == num);
+    }
+    
 }
