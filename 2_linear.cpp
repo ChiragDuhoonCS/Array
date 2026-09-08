d23e0b08d7f685460c640c50814920b084fcc382
Chirag Duhoon <chiragduhoon.cs@gmail.com>
2026-09-08 08:37:10 +0530

    linear search

--- 2_linear.cpp
+++ 2_linear.cpp
@@ -4,13 +4,20 @@ using namespace std;
 void program1() {
     int arr[] = {6,7,8,4,1};
     int n = sizeof(arr) / sizeof(arr[0]);
-    int num;
+    int num = 4;
     
 
     for (int i = 0; i < n; i++)
-    num = arr[i];
     {
-        if (arr[i] == num);
-    }
+        if (arr[i] == num) {
+        cout << "Your number is:  " << arr[i] << " at index" << endl;
+        return;
+        }
+    } 
+   
     
+}
+int main() {
+    program1();
+    return 0;
 }
