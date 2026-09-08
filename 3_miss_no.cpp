a44de1226d37eaac0bb692be5338329931218441
Chirag Duhoon <chiragduhoon.cs@gmail.com>
2026-09-08 09:15:43 +0530

    optimise 1 finding missing number

--- 3_miss_no.cpp
+++ 3_miss_no.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 using namespace std;
 
-void program1() {
+//> FINDING MISSING NUMBER
+
+ //$ |||||  BRUTE SOLUTION |||||||||
+
+void brute() {
     // Array of size 4 containing numbers from 1 to 5 (3 is missing)
     int arr[] = {1, 2, 4, 5};
     int n = 5; // The range 1 to N
@@ -28,7 +32,31 @@ void program1() {
     cout << "No missing number" << endl;
 }
 
+
+void optimise1() {
+    int arr[] = {1, 2, 4, 5};
+    int arr_size = sizeof(arr) / sizeof(arr[0]);
+    int n = arr_size + 1;
+    int sum = (n * (n+1))/2; //! total one
+    int sum2 = 0;
+
+    for (int i = 0; i < arr_size; i++)
+    {
+        sum2 = sum2 + arr[i]; //! array one
+    }
+
+    if (sum2 == sum ) {
+        printf("No missing number");
+    }
+
+    else if (sum2 != sum) {
+        printf("Missing number found %d", sum - sum2);
+    }
+    
+
+}
+
 int main() {
-    program1();
+    optimise1();
     return 0;
 }
