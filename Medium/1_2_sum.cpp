3da58608ba6c5bc3bc26a115a51bf1940efd8f86
Chirag Duhoon <chiragduhoon.cs@gmail.com>
2026-09-09 10:22:18 +0530

    [revise] 2 sum by hashing

--- Medium/1_2_sum.cpp
+++ Medium/1_2_sum.cpp
@@ -26,9 +26,34 @@ int twosum(vector<int> &arr) {
 }
 
 
+//!  BY HASHING AND MPP [BETTER SOLUTION]
+
+string twoSum(int n, vector<int> &arr, int target) {
+    map<int, int> mpp;
+    for (int i = 0; i < n; i++) {
+        int a = arr[i];
+        int more = target - a;
+        if (mpp.find(more) != mpp.end()) {
+            return "YES";
+        }
+        mpp[a] = i;
+    }
+    return "NO";
+}
+
 int main()
+{
+    int n = 5;
+    vector<int> arr = {2, 6, 5, 8, 11};
+    int target = 14;
+    string ans = twoSum(n, arr, target);
+    cout << "This is the answer: " << ans << endl;
+    return 0;
+}
+
+/*int main()
 {
     vector<int> arr = {2,6,5,8,11};
     int ans = twosum(arr);
     return 0;
-}
+}*/
