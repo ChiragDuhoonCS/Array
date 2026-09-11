2762aad8eaddcdf94f0c9c9f90049b1774cef13b
Chirag Duhoon <chiragduhoon.cs@gmail.com>
2026-09-11 18:38:17 +0530

    Belgium National Flag

--- Medium/sort_0_1_2.cpp
+++ Medium/sort_0_1_2.cpp
@@ -41,6 +41,22 @@ void better(vector<int>& nums) {
 
 
 
+void sortColors(vector<int>& nums) {
+    int low = 0, mid = 0;
+    int high = nums.size() - 1;
+
+    while (mid <= high) {
+        if (nums[mid] == 0) {
+            swap(nums[low], nums[mid]);
+            low++;
+            mid++;
+        } 
+        else if (nums[mid] == 1) {
+            mid++;
+        } 
+        else { // nums[mid] == 2
+            swap(nums[mid], nums[high]);
+            high--;
             // Note: mid is not incremented here because we need 
             // to check the value that was swapped from `high`.
         }
