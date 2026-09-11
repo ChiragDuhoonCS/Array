d77ea189b79ed241c8d00a24af05f0ffdf89bca9
Chirag Duhoon <chiragduhoon.cs@gmail.com>
2026-09-11 17:57:18 +0530

    sort an array of 0,1,2

---
+++ Medium/sort_0_1_2.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int arr[] = {0, 1, 2, 0, 1, 2,1,2,0,0,0,1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    // Sort in ascending order
+    sort(arr, arr + n);
+
+    // Output the sorted array
+    cout << "Sorted array (Ascending): ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    
+    return 0;
+}
