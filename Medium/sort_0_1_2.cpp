d5416ac0e9ceed6f3344f3faa39d0d8506b23e7d
Chirag Duhoon <chiragduhoon.cs@gmail.com>
2026-09-11 18:23:54 +0530

    better solution

--- Medium/sort_0_1_2.cpp
+++ Medium/sort_0_1_2.cpp
@@ -15,7 +15,45 @@ void brute() {
     }
 }
 
+
+void better(vector<int>& nums) {
+    int count0 = 0, count1 = 0, count2 = 0;
+
+    // Step 1: Count the number of 0s, 1s, and 2s
+    for (int i = 0; i < nums.size(); i++) {
+        if (nums[i] == 0) count0++;
+        else if (nums[i] == 1) count1++;
+        else count2++;
+    }
+
+    // Step 2: Overwrite the array based on the counts
+    int i = 0;
+    while (count0--) {
+        nums[i++] = 0;
+    }
+    while (count1--) {
+        nums[i++] = 1;
+    }
+    while (count2--) {
+        nums[i++] = 2;
+    }
+}
+
 int main() {
-    brute();
+    vector<int> nums = {2, 0, 2, 1, 1, 0};
+    
+    better(nums);
+
+    cout << "Sorted array: ";
+    for (int num : nums) {
+        cout << num << " ";
+    }
+    cout << endl;
+
     return 0;
 }
+
+/*int main() {
+    brute();
+    return 0;
+}*/
