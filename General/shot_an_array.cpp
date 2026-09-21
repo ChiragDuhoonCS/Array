14f2bf7ad2c04a8ecd79f9ff7cb049ce971e6cf0
Chirag Duhoon <chiragduhoon.cs@gmail.com>
2026-09-21 11:24:22 +0530

    sort an array

--- General/shot_an_array.cpp
+++ General/shot_an_array.cpp
@@ -1,7 +1,7 @@
-#include <iostream>
-#include <algorithm> // Required for std::sort
+#include <bits/stdc++.h>
+using namespace std;
 
-int main() {
+int sort1() {
     int arr[] = {45, 12, 89, 3, 23, 77};
     int n = sizeof(arr) / sizeof(arr[0]);
 
@@ -14,5 +14,24 @@ int main() {
         std::cout << arr[i] << " ";
     }
     
+    return 0;
+}
+
+int sort2() {
+    int arr[] = {23,56,78,34,23,44};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    sort(arr, arr + n);
+
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d , ",arr[i]);
+    }
+    return 0;
+}
+
+int main() {
+    sort2();
     return 0;
 }
