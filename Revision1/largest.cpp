30429bbe3f7afe03ad8e54cc71aa2591ccccd23f
Chirag Duhoon <chiragduhoon.cs@gmail.com>
2026-09-21 11:36:14 +0530

    largest

---
+++ Revision1/largest.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//!  BRUTE SOLUTION    TC == nlogn + n
+int large() {
+    int arr[] = {3,2,1,5,2};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    sort(arr,arr+n);
+
+    printf("%d",arr[n-1]);
+    return 0;
+}
+
+
+//!  OPTIMISE SOLUTION    TC == less than n
+
+int large2() {
+    int arr[] = {3,2,1,5,2};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int largest = arr[0]; //@ focus here
+
+    for (int i = 0; i < n; i++)
+    {
+        if(arr[i] > largest){
+            largest = arr[i]; //@here too
+            break;
+        }
+
+    }
+    printf("%d",largest);
+    return 0;
+
+
+    
+
+}
+int main() {
+    large2();
+    return 0;
+}
