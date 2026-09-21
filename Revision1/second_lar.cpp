fe0d0b1b0dea996d6c1e1f16a420c25820130287
Chirag Duhoon <chiragduhoon.cs@gmail.com>
2026-09-21 11:55:08 +0530

    SECOND LARGEST

--- Revision1/second_lar.cpp
+++ Revision1/second_lar.cpp
@@ -1,28 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int sec(int arr[],int n,int second) {
-    sort(arr,arr+n);
 
-    for (int i = n-1; i >= 0; i--)
-    {
-        if(arr[n-1] != second) {
-            second = arr[i];
+//!  BRUTE SOLUTION {SEE AGAIN}   TC == N
+int sec(int arr[], int n) {
+    sort(arr, arr + n);
+    
+    int largest = arr[n - 1];
+    int second_largest = -1; // Or a suitable default value
+    
+    // Traverse backwards to find the first element not equal to the largest
+    for (int i = n - 2; i >= 0; i--) {
+        if (arr[i] != largest) {
+            second_largest = arr[i];
             break;
         }
     }
     
-    printf("%d",second);
-    return 0;
-
+    return second_largest;
 }
 
 int main(){
-    int arr[] = {1,2,3,7,7,5};
+    int arr[] = {1, 2, 3, 7, 7, 5};
     int n = sizeof(arr) / sizeof(arr[0]);
-    int second = arr[0];
-
-    sec(arr,n,second);
+    
+    int result = sec(arr, n);
+    printf("Second largest: %d\n", result);
     return 0;
-
-} 
+}
