7aa3ff8c5284eed2d32649576f0b9a065a096e5e
Chirag Duhoon <chiragduhoon.cs@gmail.com>
2026-09-21 12:13:34 +0530

    Better sol of sec largest

--- Revision1/second_lar.cpp
+++ Revision1/second_lar.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 
-//!  BRUTE SOLUTION {SEE AGAIN}   TC == N
+//!  BRUTE SOLUTION {SEE AGAIN}   TC == nLogn + n
 int sec(int arr[], int n) {
     sort(arr, arr + n);
     
@@ -20,11 +20,32 @@ int sec(int arr[], int n) {
     return second_largest;
 }
 
+//!  BETTER SOLUTION      TC == 2N
+int sec2(int arr[],int n) {
+    int largest = arr[n - 1];
+    int second_largest = -1;
+
+    for (int i = 0; i < n; i++)
+    {
+    
+       if(arr[i] > largest) {
+        second_largest = largest; //! previous digit of largest goes into largeat when we found more largest
+        largest = arr[i];
+       }
+       else if(arr[i] > second_largest && arr[i] != largest) {
+        second_largest = arr[i];
+       }
+    }
+    return second_largest;
+
+}
+
+
 int main(){
     int arr[] = {1, 2, 3, 7, 7, 5};
     int n = sizeof(arr) / sizeof(arr[0]);
     
-    int result = sec(arr, n);
+    int result = sec2(arr, n);
     printf("Second largest: %d\n", result);
     return 0;
 }
